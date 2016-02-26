9f8ca6941f1894e54c43ff2380e1d6752a5e95ef
dantran304 <dantrandinhh@gmail.com>
2016-02-26 11:21:50 +0700

    upload doc

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,19 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class EnemyScript : MonoBehaviour {
-    private WeaponScript weapon;
+    private bool hasSpawn;
+    private WeaponScript[] weapons;
+    private MoveScript moveScript;
 	
     void Awake()
     {
-        weapon = GetComponent<WeaponScript>();
+        weapons = GetComponentsInChildren<WeaponScript>();
+        moveScript = GetComponent<MoveScript>();
     }
 	
-	// Update is called once per frame
-	void Update () {
-	if(weapon!=null && weapon.CanAttack)
+    void Start()
+    {
+        hasSpawn = false;
+
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        moveScript.enabled = false;
+
+        foreach(WeaponScript weapon in weapons)
         {
-            weapon.Attack(true);
+            weapon.enabled = false;
         }
-	}
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasSpawn == false)
+        {
+            if (GetComponent<Renderer>().isVisibleFrom(Camera.main))
+            {
+                Spawn();
+            }
+        }
+        else
+        {
+
+
+            foreach (WeaponScript weapon in weapons)
+            {
+                if (weapon != null && weapon.enabled && weapon.CanAttack)
+                {
+                    weapon.Attack(true);
+                }
+            }
+
+            if (GetComponent<Renderer>().isVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        gameObject.GetComponent<CircleCollider2D>().enabled = true;
+        moveScript.enabled = true;
+
+        foreach(WeaponScript weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
 }
